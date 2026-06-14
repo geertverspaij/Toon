bdaea3dcdf1aafb9d33013c89e6dd9796fe8a782
geertverspaij <geertlia3@gmail.com>
2026-06-14 11:47:00 +0200

    nu met node

--- api/list/index.js
+++ api/list/index.js
@@ -2,11 +2,13 @@ import fs from "fs";
 import path from "path";
 
 export default async function (req, res) {
-    const root = path.join(process.cwd(), "public"); // <-- jouw map
+    const root = process.cwd();
 
     const items = fs.readdirSync(root, { withFileTypes: true })
         .filter(d => d.isDirectory())
-        .map(d => d.name);
+        .map(d => d.name)
+        .filter(name => !name.startsWith("api"));
 
     res.json(items);
 }
+
