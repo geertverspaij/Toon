ed9e67fad826879d6887189f7b32974407f524a9
geertverspaij <geertlia3@gmail.com>
2026-06-15 18:11:31 +0200

    trigger action

--- js/index.js
+++ js/index.js
@@ -1,14 +1,2 @@
-import fs from "fs";
-import path from "path";
-
-export default async function (req, res) {
-    const root = process.cwd();
-
-    const items = fs.readdirSync(root, { withFileTypes: true })
-        .filter(d => d.isDirectory())
-        .map(d => d.name)
-        .filter(name => !name.startsWith("api"));
-
-    res.json(items);
-}
+console.log('Hierben ik');
 
